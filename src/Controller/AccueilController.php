a6ffa7d9bb9f8125cd9cccda8273e9892e368ada
bakary <bakarydiarra8509@gmail.com>
2020-12-18 23:17:28 +0100

    je continue mon exemple 2

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,8 +7,11 @@ use App\Entity\Produit;
 use App\Entity\User;
 use App\Repository\FavorisRepository;
 use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,10 +20,24 @@ class AccueilController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function index( ProduitRepository $produitRepo): Response
+    public function index(
+        ProduitRepository $produitRepo, 
+        PaginatorInterface $paginator,
+        EntityManagerInterface $em,
+        Request $request
+        
+        ): Response
     {
+        $query = $produitRepo->findAll();
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            9 /*limit per page*/
+        );
+
         return $this->render('accueil/index.html.twig', [
-            'products' => $produitRepo->findAll(),
+            'pagination' => $pagination,
         ]);
     }
 
